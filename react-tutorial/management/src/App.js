e2fc59176ebb187ac9944261a48a23aef755866b
yohanjang <bearv123@hanmail.net>
2023-02-08 11:03:04 +0900

    컴포넌트 예제

--- react-tutorial/management/src/App.js
+++ react-tutorial/management/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Custormer from './components/Customer';
+
+const customer = {
+  'name':'장요한',
+  'birthday':'90.01.02',
+  'gender':'남자',
+  'job':'대학생'
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Custormer
+        name={customer.name}
+        birthday={customer.birthday}
+        gender={customer.gender}
+        job={customer.job}
+        />
     </div>
   );
 }
