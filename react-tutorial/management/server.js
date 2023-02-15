4402d548b094acfd677f4479c637c20fb81da7d7
yohanjang <bearv123@hanmail.net>
2023-02-15 10:19:37 +0900

    마무리

--- react-tutorial/management/server.js
+++ react-tutorial/management/server.js
@@ -10,5 +10,33 @@ app.get('/api/hello', (req, res) => {
     res.send({message: "Hello Express!"});
 });
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+          'id': 1,
+          "image": 'https://placeimg.com/64/64/1',
+          'name':'장요한',
+          'birthday':'90.01.02',
+          'gender':'남자',
+          'job':'대학생'
+        },
+        {
+          'id': 2,
+          "image": 'https://placeimg.com/64/64/2',
+          'name':'홍길동',
+          'birthday':'92.01.02',
+          'gender':'여자',
+          'job':'프로그래머'
+        },
+        {
+          'id': 3,
+          "image": 'https://placeimg.com/64/64/3',
+          'name':'김철수',
+          'birthday':'85.01.02',
+          'gender':'남자',
+          'job':'세무사'
+        }
+        ]);
+});
 
+app.listen(port, () => console.log(`Listening on port ${port}`));
