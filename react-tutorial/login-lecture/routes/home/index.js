696eb5265184f2fd848bc77ee74225d323680bea
yohanjang <bearv123@hanmail.net>
2023-03-03 10:49:12 +0900

    [login-lecture] controller 분리

--- react-tutorial/login-lecture/routes/home/index.js
+++ react-tutorial/login-lecture/routes/home/index.js
@@ -3,12 +3,10 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.render("home/index")
-});
+const ctrl = require("./homeCont")
 
-router.get("/login", (req, res) => {
-    res.render("home/login")
-});
+router.get("/", ctrl.home);
+
+router.get("/login", ctrl.login);
 
 module.exports = router;
